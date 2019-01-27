e405b909a4a6ea22e35947880a381166ac22d546
Bernhard <bernhard.walter@me.com>
2019-01-27 14:29:01 +0100

    added overwriting of cadquery.Shape._repr_html_ again

--- cq_jupyter/__init__.py
+++ cq_jupyter/__init__.py
@@ -1,2 +1,17 @@
 from .cq_jupyter import Assembly, Part
 from .display import display
+import cadquery
+
+#
+# Monkey patching caqdquery to replace the _repr_html_ code
+#
+
+def _repr_html_(self):
+    """
+    Jupyter 3D representation support
+    """
+    part = Part(cadquery.CQ(self), "part", (1, 1, 0))
+    return display(Assembly("assembly", [part]), ortho=True)
+
+print("Overwriting _repr_html_ of cadquery.Shape")
+cadquery.Shape._repr_html_ = _repr_html_
